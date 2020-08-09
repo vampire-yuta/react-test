95419edfd4bea99085f75eed8924a9bcbebb2501
vampire-yuta <y-kujirai@vamdemicsystem.black>
2020-08-09 19:22:45 +0900

    quita api get

--- src/App.js
+++ src/App.js
@@ -1,42 +1,7 @@
-import React, { Component, Fragment } from 'react';
-import axios from 'axios';
-import User from './App.css'
+import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      userDatas:[]
-    };
-  }
-  componentWillMount(){
-    const request = axios.create({
-      baseURL: 'https://api.github.com'
-    })
-    request.get('/users/vampire-yuta')
-      .then(res => {
-        this.setState({
-          userDatas: res.data
-        });
-      })
-  }
-  render() {
-    return (
-      <Fragment>
-      <header className="header"></header>
-      <div className="user">
-        <p className="user_img"><img src={this.state.userDatas.avatar_url}/ ></p>
-        <div className="user_name">{this.state.userDatas.name}</div>
-        <div className="user_id">{this.state.userDatas.login}</div>
-        <div className="user_followArea">
-          <div className="user_followers">followers:{this.state.userDatas.followers}</div> 
-          <div className="user_following">following:{this.state.userDatas.following}</div>
-        </div>
-        <div className="created_at">{this.state.userDatas.created_at}</div>
-      </div>
-      </Fragment>
-    );
-  }
+    render() {
+        return <div><h1>Hello</h1></div>
+    }
 }
-
-export default App;
